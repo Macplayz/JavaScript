cc6035b032374e59594ff34066f2baf18f08d99c
Macplayz <88567205+Macplayz@users.noreply.github.com>
2025-01-19 14:07:02 +0000

    Conversion Datatypes

---
+++ 01_Basics /03_DataTypeConversion.js
@@ -0,0 +1,23 @@
+let score = "33a"
+
+console.log(typeof score)
+
+let ValueInNum = Number(score)
+console.log(typeof (ValueInNum))
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let isloggedin = 1
+
+let isloggedincheck = Boolean(isloggedin)
+console.log(typeof (isloggedincheck))
+
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
