df59e9de03b643e709d25f0239124323799330b7
Macplayz <88567205+Macplayz@users.noreply.github.com>
2025-01-19 13:27:42 +0000

    DataTypes

---
+++ 01_Basics /02_DataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //Treat all Js code for the newer version 
+
+let name = "JavaScript"
+let age = 18 
+let islogin = false
+
+//Number - 2^53
+//bigint
+//String - ""
+//boolean - True/False
+//null - standalone value
+//undefined - not assigned value
+//symbol - unique 
+
+//object - special 
