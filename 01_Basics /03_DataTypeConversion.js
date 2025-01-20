f998b4735c6b970eda2e2c30ef1305163f560ac5
Macplayz <88567205+Macplayz@users.noreply.github.com>
2025-01-20 10:17:07 +0000

    DataTypeConversions

--- 01_Basics /03_DataTypeConversion.js
+++ 01_Basics /03_DataTypeConversion.js
@@ -1,9 +1,9 @@
 let score = "33a"
 
-console.log(typeof score)
+// console.log(typeof score)
 
 let ValueInNum = Number(score)
-console.log(typeof (ValueInNum))
+// console.log(typeof (ValueInNum))
 
 // "33" => 33
 // "33abc" => NaN
@@ -13,7 +13,7 @@ console.log(typeof (ValueInNum))
 let isloggedin = 1
 
 let isloggedincheck = Boolean(isloggedin)
-console.log(typeof (isloggedincheck))
+// console.log(typeof (isloggedincheck))
 
 //  console.log(booleanIsLoggedIn);
 
@@ -21,3 +21,42 @@ console.log(typeof (isloggedincheck))
 // "" => false
 // "hitesh" => true
 
+//Operations//
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " World";
+
+let str3=str1+str2;
+// console.log(str3);
+
+let num1,num2,num3
+
+num1=4
+num2=5
+num3=num1/num2
+
+// console.log(num3)
+
+//Postfix
+
+// let x=4
+// let y=x++
+
+// console.log(`x:${x}, y:${y}`)
+
+//x=5,y=4
+
+//Prefix
+
+let j=4
+let k= ++j
+
+console.log(`x:${j}, y:${k}`)
+
+//x=5 y=5 
