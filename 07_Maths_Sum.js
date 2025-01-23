74191055ce685915471b825f58bfd5531724db9d
Macplayz <88567205+Macplayz@users.noreply.github.com>
2025-01-23 15:21:04 +0000

    Maths

---
+++ 07_Maths_Sum.js
@@ -0,0 +1,14 @@
+// const score = 400 
+// console.log(score);
+
+// const newscore = new Number(score)
+// console.log(newscore);
+
+// console.log(newscore.toString().length);
+// console.log(newscore.toFixed(2));
+
+const Number = 23.826454
+console.log(Number.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString());
