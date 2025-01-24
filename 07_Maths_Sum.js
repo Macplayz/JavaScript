96fed83c59342eb531394ee305b8482841c39644
Macplayz <88567205+Macplayz@users.noreply.github.com>
2025-01-24 04:09:39 +0000

    Maths and Num

--- 07_Maths_Sum.js
+++ 07_Maths_Sum.js
@@ -1,5 +1,5 @@
 // const score = 400 
-// console.log(score);
+// // console.log(score);
 
 // const newscore = new Number(score)
 // console.log(newscore);
@@ -7,8 +7,27 @@
 // console.log(newscore.toString().length);
 // console.log(newscore.toFixed(2));
 
-const Number = 23.826454
-console.log(Number.toPrecision(3));
+// const Number = 23.826454
+// console.log(Number.toPrecision(3));
 
-const hundreds = 1000000
-console.log(hundreds.toLocaleString());
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+
+// //++++++++++++ Maths +++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4));
+
+console.log(Math.round(5.4))
+console.log(Math.floor(4.5))
+console.log(Math.ceil(4.5))
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log((Math.random()*10) + 1)
+console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
