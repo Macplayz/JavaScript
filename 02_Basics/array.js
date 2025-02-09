3dddc007508123e8568525fd1fccdd37668c6fb0
Macplayz <88567205+Macplayz@users.noreply.github.com>
2025-02-09 07:15:04 +0000

    Array_Basic_01

---
+++ 02_Basics/array.js
@@ -0,0 +1,47 @@
+//Array
+
+const arr = [0,1,2,3,4,5]
+
+const cars=["Mercedes Benz","Mclaren","Lambogirhni","Audi"]
+
+// cars.push("Nissan");
+
+// console.log(cars.sort());
+
+// cars.pop()
+
+// console.log(arr[2]);
+
+// arr.unshift(3)
+// arr.shift()
+
+// console.log(arr)
+
+// console.log(cars)
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(9));
+
+// const newarr = arr.join()
+
+// console.log(arr);
+// console.log(newarr);
+// console.log(typeof newarr);
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
