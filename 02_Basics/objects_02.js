e2afb14b1ab5e1e3df2df9def511960e8b159b62
Macplayz <88567205+Macplayz@users.noreply.github.com>
2025-02-10 10:21:12 +0000

    Objects Part 2

---
+++ 02_Basics/objects_02.js
@@ -0,0 +1,88 @@
+//literal Object 
+// const tinderuser = {}
+
+// console.log(tinderuser);
+
+//Singleton Object 
+const tinderuser = new Object()
+
+// console.log(tinderuser);
+
+tinderuser.id = "123abc"
+tinderuser.name = "Sammy"
+tinderuser.isloggined = false
+
+// console.log(tinderuser)
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+//const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4);
+// console.log(obj3);
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
